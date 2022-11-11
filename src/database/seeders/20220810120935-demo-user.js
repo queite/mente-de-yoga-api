6939e52f5bfe39a6ed16d5f51ae5efbdf044c3ea
Queite <queitesc@gmail.com>
2022-11-11 08:01:15 -0300

    fix: :bug: fix typescript error on request.user

--- src/database/seeders/20220810120935-demo-user.js
+++ src/database/seeders/20220810120935-demo-user.js
@@ -1,30 +1,24 @@
-const bcrypt = require('bcrypt')
+const bcrypt = require("bcrypt");
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    return queryInterface.bulkInsert('users', [
+    return queryInterface.bulkInsert("users", [
       {
-        name: 'Doe Admin',
-        email: 'example@example.com',
-        userPermission: 'admin',
-        password: bcrypt.hashSync('123456', 3),
+        name: "Doe Admin",
+        email: "example@example.com",
+        userPermission: "admin",
+        password: bcrypt.hashSync("123456", 3),
       },
       {
-        name: 'Doe Read',
-        email: 'example1@example.com',
-        userPermission: 'read',
-        password:  bcrypt.hashSync('123456', 3),
-      },
-      {
-        name: 'Doe Write',
-        email: 'example2@example.com',
-        userPermission: 'write',
-        password: bcrypt.hashSync('123456', 3),
+        name: "Doe Student",
+        email: "student@example.com",
+        userPermission: "student",
+        password: bcrypt.hashSync("123456", 3),
       },
     ]);
   },
 
   async down(queryInterface) {
-    return queryInterface.bulkDelete('users', null, {});
+    return queryInterface.bulkDelete("users", null, {});
   },
 };
--- src/middlewares/authorizationMiddleware.ts
+++ src/middlewares/authorizationMiddleware.ts
@@ -3,12 +3,12 @@ import HttpException from '../errors/httpException';
 
 function authorizationMiddleware(
   req: Request,
-  res: Response,
+  _res: Response,
   next:NextFunction,
 ) {
-  const { userInfo } = req;
+  const { user } = req;
 
-  if (userInfo.userPermission !== 'admin') {
+  if (user?.userPermission !== 'admin') {
     throw new HttpException(401, 'Unauthorized');
   }
 
