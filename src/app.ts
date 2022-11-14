bb08007b3bd7552e32917c2cd7054c53891524e9
Queite <queitesc@gmail.com>
2022-11-14 08:59:12 -0300

    build: :heavy_plus_sign: add cors

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,19 @@
+import cors from 'cors';
 import express from 'express';
 import 'express-async-errors';
-import authRoute from './routes/authRoute';
 import errorMiddleware from './middlewares/errorMiddleware';
+import authRoute from './routes/authRoute';
 import userRoute from './routes/userRoute';
 
 const app = express();
 app.use(express.json());
+
+// const allowedOrigins = ['http://localhost:3000'];
+
+// const options: cors.CorsOptions = {
+//   origin: allowedOrigins,
+// };
+app.use(cors());
 
 app.use('/login', authRoute);
 app.use('/users', userRoute);
